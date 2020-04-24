8247cc0bd70be20f1b680c58c35088bf9420b5d5
Андрей Григорьев <greegav@gmail.com>
2020-04-24 16:26:53 +0300

    Initial commit

---
+++ src/main/java/com/pluralsight/model/Speaker.java
@@ -0,0 +1,57 @@
+package com.pluralsight.model;
+
+public class Speaker {
+
+    private String firstName;
+    private String lastName;
+
+    public Speaker() {
+    }
+
+    public Speaker(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Speaker{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Speaker speaker = (Speaker) o;
+
+        if (firstName != null ? !firstName.equals(speaker.firstName) : speaker.firstName != null) return false;
+        return lastName != null ? lastName.equals(speaker.lastName) : speaker.lastName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
+    }
+}
